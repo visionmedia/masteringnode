44f30a28a4b3402a6fc27355d0baa49d423502f3
Tj Holowaychuk <tj@vision-media.ca>
2010-08-02 23:07:51 -0700

    Misc

--- src/http-hello-world/app.js
+++ src/http-hello-world/app.js
@@ -1,10 +1,9 @@
 
-var sys = require('sys'),
-    http = require('http');
+var http = require('http');
 
 http.createServer(function(req, res){
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(3000);
 
-sys.puts('Server running at http://127.0.0.1:3000/');
+console.log('Server running at http://localhost:3000/');
