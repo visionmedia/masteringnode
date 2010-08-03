add2782f1964954a701edb6b865d425a419e0308
Tj Holowaychuk <tj@vision-media.ca>
2010-08-02 22:44:36 -0700

    Fixed math example

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,12 +1,11 @@
 
-var sys = require('sys'),
-    utils = require('./utilities');
+var utils = require('./utilities');
 
 var a = { one: 1 };
 var b = { two: 2 };
 utils.merge(a, b);
-sys.p(a);
+console.dir(a)
 
 var math = require('./math');
-sys.puts(math.add(3, 1));
-sys.puts(math.sub(3, 1));
+console.log(math.add(3, 1));
+console.log(math.sub(3, 1));
--- src/modules/math/index.js
+++ src/modules/math/index.js
@@ -1,5 +1,5 @@
 
-exports = {
+module.exports = {
     add: require('./add'),
     sub: require('./sub')
 };
